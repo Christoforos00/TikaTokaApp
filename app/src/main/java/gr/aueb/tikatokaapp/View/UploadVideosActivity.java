7ec0a7001322b95a5f0c98076584c0d88a6697bc
zaaachos <gzachos5@gmail.com>
2021-06-11 13:24:27 +0300

    save recorded videos to specific folder

--- app/src/main/java/gr/aueb/tikatokaapp/View/UploadVideosActivity.java
+++ app/src/main/java/gr/aueb/tikatokaapp/View/UploadVideosActivity.java
@@ -3,6 +3,8 @@ package gr.aueb.tikatokaapp.View;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
@@ -16,6 +18,12 @@ import androidx.core.content.FileProvider;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
 
 import gr.aueb.tikatokaapp.Core.ConnectedAppNode;
@@ -24,7 +32,7 @@ import gr.aueb.tikatokaapp.R;
 import gr.aueb.tikatokaapp.View.fragmentList.VideoListFragment;
 
 
-public class UploadVideosActivity extends AppCompatActivity implements VideoListFragment.OnListFragmentInteractionListener{
+public class UploadVideosActivity extends AppCompatActivity implements VideoListFragment.OnListFragmentInteractionListener {
 
     public static final int CAMERA_PERMISSION_CODE = 100;
 
@@ -33,7 +41,7 @@ public class UploadVideosActivity extends AppCompatActivity implements VideoList
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload_videos);
 
-        ((ImageButton) findViewById(R.id.rec_button)).setOnClickListener(v -> onRec() );
+        ((ImageButton) findViewById(R.id.rec_button)).setOnClickListener(v -> onRec());
 
         if (findViewById(R.id.fragment_container) != null) {
 
@@ -62,25 +70,86 @@ public class UploadVideosActivity extends AppCompatActivity implements VideoList
         return new ArrayList<Value>();
     }
 
-    public void onRec(){
-        Intent intent = new Intent( MediaStore.ACTION_VIDEO_CAPTURE);
-        intent.putExtra(MediaStore.EXTRA_OUTPUT,Environment.getExternalStorageDirectory() );
-        intent.putExtra(MediaStore.EXTRA_DURATION_LIMIT , 60);
+    public void onRec() {
+        Intent intent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_DURATION_LIMIT, 60);
         startActivityForResult(intent, 1);
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if ( resultCode == RESULT_OK && requestCode ==1){
+    private String videoPath = "";
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+
+            Uri vid = data.getData();
+            videoPath = getRealPathFromURI(vid);
+            String newPath = ConnectedAppNode.getAppNode().getPubDir();
+            try {
+                transferVideo(new File(videoPath), new File(newPath+"/videos/test_video.mp4"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            deleteRecursive(new File(videoPath));
+        }
+
+
+    }
+
+    public String getRealPathFromURI(Uri contentUri) {
+        String[] proj = {MediaStore.Images.Media.DATA};
+        Cursor cursor = managedQuery(contentUri, proj, null, null, null);
+        int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        cursor.moveToFirst();
+        return cursor.getString(column_index);
+    }
+
+    public static void transferVideo(File sourceLocation, File targetLocation)
+            throws IOException {
+
+        if (sourceLocation.isDirectory()) {
+            if (!targetLocation.exists()) {
+                targetLocation.mkdir();
+            }
+
+            String[] children = sourceLocation.list();
+            for (int i = 0; i < sourceLocation.listFiles().length; i++) {
+
+                transferVideo(new File(sourceLocation, children[i]),
+                        new File(targetLocation, children[i]));
+            }
+        } else {
+
+            InputStream in = new FileInputStream(sourceLocation);
+
+            OutputStream out = new FileOutputStream(targetLocation);
+
+            // Copy the bits from instream to outstream
+            byte[] buf = new byte[1024];
+            int len;
+            while ((len = in.read(buf)) > 0) {
+                out.write(buf, 0, len);
+            }
+            in.close();
+            out.close();
         }
 
     }
 
-    public void getCameraPermission(){
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED){
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA} , CAMERA_PERMISSION_CODE  );
+    public static void deleteRecursive(File fileOrDirectory) {
+
+        if (fileOrDirectory.isDirectory())
+            for (File child : fileOrDirectory.listFiles())
+                deleteRecursive(child);
+
+        fileOrDirectory.delete();
+
+    }
+
+    public void getCameraPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, CAMERA_PERMISSION_CODE);
         }
     }
 }
